eb5dfdaecbe81844355a79bcc42c1e286c69480d
opiirone24 <opiirone24@student.oulu.fi>
2025-11-03 10:31:37 +0200

    Alpha 0.3.3 (hat_imu_ex)

--- examples/hat_imu_ex/src/main.c
+++ examples/hat_imu_ex/src/main.c
@@ -13,19 +13,17 @@
 void imu_task(void* pvParameters) {
     (void)pvParameters;
 
-    float ax, ay, az, gx, gy, gz, t;
+    float gx, gy, gz;
     // Setting up the sensor. 
     if (init_ICM42670() == 0) {
         printf("ICM-42670P initialized successfully!\n");
         if (ICM42670_start_with_default_values() != 0) {
-            printf("ICM-42670P could not initialize accelerometer or gyroscope");
+            printf("ICM-42670P could not initialize gyroscope");
         }
         int _enablegyro = ICM42670_enable_accel_gyro_ln_mode();
         printf ("Enable gyro: %d\n",_enablegyro);
         int _gyro = ICM42670_startGyro(ICM42670_GYRO_ODR_DEFAULT, ICM42670_GYRO_FSR_DEFAULT);
         printf ("Gyro return:  %d\n", _gyro);
-        int _accel = ICM42670_startAccel(ICM42670_ACCEL_ODR_DEFAULT, ICM42670_ACCEL_FSR_DEFAULT);
-        printf ("Accel return:  %d\n", _accel);
     }
     else {
         printf("Failed to initialize ICM-42670P.\n");
@@ -33,9 +31,9 @@ void imu_task(void* pvParameters) {
     // Start collection data here. Infinite loop. 
     while (1)
     {
-        if (ICM42670_read_sensor_data(&ax, &ay, &az, &gx, &gy, &gz, &t) == 0) {
+        if (ICM42670_read_sensor_data(&gx, &gy, &gz) == 0) {
 
-            printf("Accel: X=%f, Y=%f, Z=%f | Gyro: X=%f, Y=%f, Z=%f| Temp: %2.2f°C\n", ax, ay, az, gx, gy, gz, t);
+            printf("Gyro: X=%f, Y=%f, Z=%f\n",gx, gy, gz);
 
         }
         else {
@@ -60,7 +58,7 @@ int main() {
     init_hat_sdk();
     sleep_ms(300); // Wait some time so initialization of USB and hat is done.
     init_led();
-    printf("Start acceleration test\n");
+    printf("Start test\n");
 
     TaskHandle_t hIMUTask = NULL;
 
