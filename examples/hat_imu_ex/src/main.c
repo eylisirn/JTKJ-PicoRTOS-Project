dfc4d5c1ed0a14ebb573be9ed2a078cddc92495b
opiirone24 <opiirone24@student.oulu.fi>
2025-11-03 10:05:51 +0200

    Alpha 0.3.0 (hat_imu_ex, kokeilu)

--- examples/hat_imu_ex/src/main.c
+++ examples/hat_imu_ex/src/main.c
@@ -1,4 +1,4 @@
-
+// IMU-SENSORI (PLACEHOLDER KOODI, REVITTY ESIMERKKEJÄ "hat_imu_ex" TIEDOSTOSTA
 #include <stdio.h>
 #include <pico/stdlib.h>
 
@@ -8,16 +8,16 @@
 
 #include <tkjhat/sdk.h>
 
+//#define I2C_PORT i2c0
 
-
-void imu_task(void *pvParameters) {
+void imu_task(void* pvParameters) {
     (void)pvParameters;
-    
+
     float ax, ay, az, gx, gy, gz, t;
     // Setting up the sensor. 
     if (init_ICM42670() == 0) {
         printf("ICM-42670P initialized successfully!\n");
-        if (ICM42670_start_with_default_values() != 0){
+        if (ICM42670_start_with_default_values() != 0) {
             printf("ICM-42670P could not initialize accelerometer or gyroscope");
         }
         /*int _enablegyro = ICM42670_enable_accel_gyro_ln_mode();
@@ -26,17 +26,19 @@ void imu_task(void *pvParameters) {
         printf ("Gyro return:  %d\n", _gyro);
         int _accel = ICM42670_startAccel(ICM42670_ACCEL_ODR_DEFAULT, ICM42670_ACCEL_FSR_DEFAULT);
         printf ("Accel return:  %d\n", _accel);*/
-    } else {
+    }
+    else {
         printf("Failed to initialize ICM-42670P.\n");
     }
     // Start collection data here. Infinite loop. 
     while (1)
     {
         if (ICM42670_read_sensor_data(&ax, &ay, &az, &gx, &gy, &gz, &t) == 0) {
-            
+
             printf("Accel: X=%f, Y=%f, Z=%f | Gyro: X=%f, Y=%f, Z=%f| Temp: %2.2f°C\n", ax, ay, az, gx, gy, gz, t);
 
-        } else {
+        }
+        else {
             printf("Failed to read imu data\n");
         }
         vTaskDelay(pdMS_TO_TICKS(500));
@@ -47,11 +49,16 @@ void imu_task(void *pvParameters) {
 int main() {
     stdio_init_all();
     // Uncomment this lines if you want to wait till the serial monitor is connected
-    while (!stdio_usb_connected()){
+    while (!stdio_usb_connected()) {
         sleep_ms(10);
     }
-    init_hat_sdk();
-    sleep_ms(300); //Wait some time so initialization of USB and hat is done.
+    //i2c_init(I2C_PORT, 400 * 1000);
+    //gpio_set_function(12, GPIO_FUNC_I2C);
+    //gpio_set_function(13, GPIO_FUNC_I2C);
+    //gpio_pull_up(12);
+    //gpio_pull_up(13);
+    //1init_hat_sdk();
+    sleep_ms(300); // Wait some time so initialization of USB and hat is done.
     init_led();
     printf("Start acceleration test\n");
 
@@ -64,4 +71,3 @@ int main() {
 
     return 0;
 }
-
