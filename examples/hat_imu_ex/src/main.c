320c76b641dff5e31e04588cded923bdf91d9ff0
opiirone24 <opiirone24@student.oulu.fi>
2025-11-14 08:30:34 +0200

    Alpha 0.7.1 (korjattu)

--- examples/hat_imu_ex/src/main.c
+++ examples/hat_imu_ex/src/main.c
@@ -116,7 +116,6 @@ void imu_task(void* pvParameters) {
             button2_pressed_flag = false;
 
             if (message_index < MESSAGE_BUFFER_SIZE - 1) {
-                message_buffer[message_index++] = '';
                 message_buffer[message_index] = '\0';
                 printf("%c\n", " ");
             }
