745779b59c064137cbcb6dcc2b77616289707355
opiirone24 <opiirone24@student.oulu.fi>
2025-11-14 08:39:06 +0200

    Alpha 0.7.4 (kokeilu)

--- examples/hat_imu_ex/src/main.c
+++ examples/hat_imu_ex/src/main.c
@@ -2,36 +2,25 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
-// Button pin (use your real pin)
 #define BUTTON_PIN 2
-
-// Morse version of "HELLO!"
-const char* MORSE_MESSAGE =
-".... "   // H
-". "      // E
-".-.. "   // L
-".-.. "   // L
-"--- "    // O
-"-.-.--"  // !
-"\n";     // End with newline
 
 int main() {
     stdio_init_all();
-    sleep_ms(500);  // Let USB CDC connect
+    sleep_ms(500);
 
-    // Initialize button
     gpio_init(BUTTON_PIN);
     gpio_set_dir(BUTTON_PIN, GPIO_IN);
-    gpio_pull_up(BUTTON_PIN); // Active low
+    gpio_pull_up(BUTTON_PIN);
 
-    printf("Ready. Press button to send HELLO! in Morse.\n");
+    printf("READY\n");
+
+    const char* morse = ".... . .-.. .-.. --- -.-.--\n";
 
     while (1) {
-        // Button pressed? (active LOW)
         if (!gpio_get(BUTTON_PIN)) {
-            printf("%s", MORSE_MESSAGE);
-            sleep_ms(500); // Debounce + avoid spamming
-            while (!gpio_get(BUTTON_PIN)); // Wait for release
+            printf("%s", morse);
+            sleep_ms(300);
+            while (!gpio_get(BUTTON_PIN));
         }
     }
 }
