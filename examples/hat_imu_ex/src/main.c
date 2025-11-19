6286be180623f68562b243492408886f5a4ee99e
eylisirn <eylisirn23@student.oulu.fi>
2025-11-19 14:57:43 +0200

    Alpha 0.9.6 (korjattu)

--- examples/hat_imu_ex/src/main.c
+++ examples/hat_imu_ex/src/main.c
@@ -75,7 +75,7 @@ void imu_task(void* pvParameters) {
             clear_display();
             if (morse_index > 0) {
                 write_text(morse_buffer);
-                printf("Tämän hetkinen bufferi: %s\n", morse_buffer)
+                printf("Tämän hetkinen bufferi: %s\n", morse_buffer);
             }
             else {
                 write_text(" ");
